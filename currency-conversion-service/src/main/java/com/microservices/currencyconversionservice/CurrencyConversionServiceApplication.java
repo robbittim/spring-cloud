d5c859008a3fb3171de2a4f574c07e3819b46d4d
tim <robbittim@gmail.com>
2022-05-09 16:29:06 -0500

    more microservices

--- currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,8 +1,11 @@
 package com.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class CurrencyConversionServiceApplication {
 
